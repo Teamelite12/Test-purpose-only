e59a45e4e81787cf18682e9db7bdaca5aeef1de8
Mamidi Lakshman Mohan Manikanta <131143616+mlakshmanm@users.noreply.github.com>
2024-08-27 18:27:41 +0530

    Update sample.c

--- sample.c
+++ sample.c
@@ -26,3 +26,33 @@ ldkbvn'd,vhjd 'b
 '; d;'
 b .dklvbl b'zdbrgik,ldkv]
 WSE:D><k l
+
+rkgjhdnrkrgbe
+drbdnrbodrnbvdf
+bdkgmb
+dgndkrb
+dnbd'
+b';;dnhb
+d;lhobndd,rgvfnjb df
+';l skv.
+dkfvbnd;/v/lkd v;'d;][vod'v
+ldkbvn'd,vhjd 'b
+.b,ld.b
+'; d;'
+b .dklvbl b'zdbrgik,ldkv]
+WSE:D><k l
+rkgjhdnrkrgbe
+drbdnrbodrnbvdf
+bdkgmb
+dgndkrb
+dnbd'
+b';;dnhb
+d;lhobndd,rgvfnjb df
+';l skv.
+dkfvbnd;/v/lkd v;'d;][vod'v
+ldkbvn'd,vhjd 'b
+.b,ld.b
+'; d;'
+b .dklvbl b'zdbrgik,ldkv]
+WSE:D><k l
+
